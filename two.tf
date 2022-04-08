74b1515600594a91fd426de0610f1849572adbeb
pavansw <pavanwankhade4u@gmail.com>
2022-04-08 15:49:59 +0530

    two.tf is not not fuctioning

--- two.tf
+++ two.tf
@@ -1,6 +1,6 @@
 
-resource "aws_iam_user" "usersCreate1" {
-count = 5
-name = "accenture${count.index}"
-}
+#resource "aws_iam_user" "usersCreate1" {
+#count = 5
+#name = "accenture${count.index}"
+#}
 
