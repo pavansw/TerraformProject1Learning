859e1d66e7cb61c5411d26eb892da46af2c26645
pavanswankhade <pavanwankhade4u@gmail.com>
2022-04-08 10:08:27 +0000

    New update on 8thApr by Devops team

---
+++ credentials.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+region = "us-east-1"
+access_key = "AKIAYIMEDZZZKGNPEMPI"
+secret_key = "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"
+}
+
--- main.tf
+++ main.tf
@@ -1,8 +1,22 @@
 resource "aws_vpc" "pavan-vpc" {
-cidr_block = "21.21.0.0/16"
-instance_tenancy = "default"
-tags = {
-Name="pavan-vpc"
+	cidr_block = "21.21.0.0/16"
+	instance_tenancy = "default"
+	tags = {
+		Name="pavan-vpc"
+	}
 }
+
+resource "aws_internet_gateway" "pavan-gateway" {
+	vpc_id = "${aws_vpc.pavan-vpc.id}"
+	tags = {
+		Name="pavan-gateway"
+	}
 }
+
+resource "aws_route" "pavan-route" {
+	destination_cidr_block = "0.0.0.0/0"
+	gateway_id = "${aws_internet_gateway.pavan-gateway.id}"
+	route_table_id = "${aws_vpc.pavan-vpc.main_route_table_id}"
+}
+
 
